5cc10a853f0de627b64d9ff96768de5f2233cbbd
Alex Bispo <alexbispo.artes@gmail.com>
2016-06-02 08:29:49 -0300

    Exercises 2 and 3.

--- src/brave_clojure_exercises_chapter_3/core.clj
+++ src/brave_clojure_exercises_chapter_3/core.clj
@@ -1,6 +1,25 @@
 (ns brave-clojure-exercises-chapter-3.core
   (:gen-class))
 
+(defn exercise-one
+  "Use the str, vector, list, hash-map, and hash-set functions."
+  []
+  (println "I should use the str, vector, list, hash-map, and hash-set functions."))
+
+(defn exercise-two
+  "Write a function that takes a number and adds 100 to it"
+  [number]
+  (+ number 100))
+
+(defn dec-maker
+  "Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+   Example:
+    (def dec9 (dec-maker 9))
+    (dec9 10)
+    => 1"
+  [dec-by]
+  #(- % dec-by))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
