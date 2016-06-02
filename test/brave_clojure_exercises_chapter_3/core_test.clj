4f576b774118a44428cc7beb50d8f4a83ccd27e3
Alex Bispo <alexbispo.artes@gmail.com>
2016-06-02 07:25:48 -0300

    Start project.

---
+++ test/brave_clojure_exercises_chapter_3/core_test.clj
@@ -0,0 +1,7 @@
+(ns brave-clojure-exercises-chapter-3.core-test
+  (:require [clojure.test :refer :all]
+            [brave-clojure-exercises-chapter-3.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
